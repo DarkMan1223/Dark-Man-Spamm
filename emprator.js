99e0dc5ba23438172c82cb67525213570ce91074
Glitch (hello-express) <none>
2020-11-19 17:08:14 +0000

    👌⚾️ Checkpoint ./server.js:17760135/134 ./package.json:17760135/98

--- emprator.js
+++ emprator.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = ['Your ID']///id account
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
 const client6 = new Discord.Client();
-
-
+/////برۆ بەشی env لەوی کۆماند دانی
+///////token accuont
 client.login("");
 client2.login("");
 client3.login("");
@@ -44,11 +44,11 @@ if (message.content === '1') {
 });
 
 client3.on('message', message => {
-if (message.content === '$spam') {
+if (message.content === '1') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 9000; x++) {
-        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+        message.channel.send(process.env.command)
           .then(m => {
             count++;
           })
