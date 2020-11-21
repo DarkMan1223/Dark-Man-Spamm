a8699877eb38641749fe0bcd80c0e34176762ee5
Glitch (antique-eastern-carpet) <none>
2020-11-21 19:40:37 +0000

    👲🐒 Checkpoint ./server.js:25178362/150

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['772965344926498836']
+const myid = ['779790578225250324']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
@@ -12,8 +12,8 @@ client.login("Nzc4MjMzMTgxNDQyNjcwNjIy.X7ln7w.R9ZllQ2jqILM2nhsXGZaOR_8Fzo");
 client2.login("Nzc5NjQwOTM3MTM5MjczNzI4.X7loRQ.r8FzUtY5ZQU3ntwrTcmbbNNMx4s");
 client3.login("Nzc5NjQxMzczODM3MzYxMTcy.X7logg.oJir2uQCiQbj9xO3Hg4AcJEp7mc");
 client4.login("Nzc5Nzg5MjQyNjY2MzE5OTAz.X7lpmg.0VjdP7k4XMijLoElpc5w3m-ltuw");
-client5.login("");
-client6.login("");
+client5.login("Nzc5NzkwNTc4MjI1MjUwMzI0.X7lquw.f_Zjr87-v1KSDQ05XOnQEi8DR38");
+client6.login("Nzc5Nzg5OTA2NDQ3ODI2OTQ0.X7lqJQ.TD46gceuvRPtAX3EM_DbomWpO6o");
 
 
 client.on('message', message => {
