d00ffa277f8b997e5decdc68d576018cde8caece
Glitch (antique-eastern-carpet) <none>
2020-11-21 20:49:18 +0000

    👝☔️ Checkpoint ./server.js:25178362/442

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['772965344926498836']
+const myid = ['THE MAIN ID']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
@@ -8,13 +8,13 @@ const client5 = new Discord.Client();
 const client6 = new Discord.Client();
 
 
-client.login("Nzc4MjMzMTgxNDQyNjcwNjIy.X7ln7w.R9ZllQ2jqILM2nhsXGZaOR_8Fzo");
-client2.login("Nzc5NjQwOTM3MTM5MjczNzI4.X7loRQ.r8FzUtY5ZQU3ntwrTcmbbNNMx4s");
-client3.login("Nzc5NjQxMzczODM3MzYxMTcy.X7logg.oJir2uQCiQbj9xO3Hg4AcJEp7mc");
-client4.login("Nzc5Nzg5MjQyNjY2MzE5OTAz.X7lpmg.0VjdP7k4XMijLoElpc5w3m-ltuw");
-client5.login("Nzc5NzkwNTc4MjI1MjUwMzI0.X7lquw.f_Zjr87-v1KSDQ05XOnQEi8DR38");
-client6.login("Nzc5Nzg5OTA2NDQ3ODI2OTQ0.X7lqJQ.TD46gceuvRPtAX3EM_DbomWpO6o");
-client.login("Nzc5Nzk4MDM5OTM3MTU1MTEy.X7lxvQ.26C-jqKq5ham3e3ygifN7unA0nw");
+client.login("");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+client.login("");
 
 client.on('message', message => {
 if (message.content === '$spam') {
