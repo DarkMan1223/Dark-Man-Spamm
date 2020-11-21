5001dcedd6b4b929fbb79fae46c6b46ec41c7773
Glitch (antique-eastern-carpet) <none>
2020-11-21 19:51:57 +0000

    🎤🐧 Checkpoint ./server.js:25178362/96

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['779790578225250324']
+const myid = ['772965344926498836']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
