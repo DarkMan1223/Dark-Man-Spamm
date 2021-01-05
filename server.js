3c3331a6190a6867a43995c23bfea3e3ecd12fdf
Glitch (antique-eastern-carpet) <none>
2021-01-05 21:58:56 +0000

    🚌📖 Checkpoint ./server.js:27010245/483

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const client5 = new Discord.Client();
 const client6 = new Discord.Client();
 
 
-client.login("Nzc4MjMzMTgxNDQyNjcwNjIy.X7ln7w.R9ZllQ2jqILM2nhsXGZaOR_8Fzo");
-client2.login("Nzc5NjQwOTM3MTM5MjczNzI4.X7loRQ.r8FzUtY5ZQU3ntwrTcmbbNNMx4s");
-client3.login("Nzc5NjQxMzczODM3MzYxMTcy.X7logg.oJir2uQCiQbj9xO3Hg4AcJEp7mc");
-client4.login("Nzc5Nzg5MjQyNjY2MzE5OTAz.X7lpmg.0VjdP7k4XMijLoElpc5w3m-ltuw");
-client5.login("Nzc5Nzg5OTA2NDQ3ODI2OTQ0.X7lqJQ.TD46gceuvRPtAX3EM_DbomWpO6o");
-client6.login("Nzc5NzkwNTc4MjI1MjUwMzI0.X7lquw.f_Zjr87-v1KSDQ05XOnQEi8DR38");
-client.login("Nzc5Nzk4MDM5OTM3MTU1MTEy.X7lxvQ.26C-jqKq5ham3e3ygifN7unA0nw");
+client.login("");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+client.login("");
 
+//// By : Wolf
 client.on('message', message => {
 if (message.content === '$spam') {
       let count = 0;
@@ -101,4 +102,4 @@ if (message.content === '$spam') {
 });
 
 
-///
+///////By : WOLF
