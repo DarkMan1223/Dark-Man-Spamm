866cd7665fa759ef6886b9c33f44cfc3ffecf45d
Glitch (antique-eastern-carpet) <none>
2021-02-12 14:49:29 +0000

    🍶🏐 Checkpoint ./server.js:28475532/3360

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const Discord = require("discord.js");
-const myid = ['772965344926498836']
+const myid = ['786232962579431424']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
 const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
 
-
-client.login("NzkyODk0MTgwNTE4MjY0ODQz.X-nTCA.4_csBCXTSHrrM2R7aSpFYEz6x6c");
-client2.login("NzkzMDk4NjMzNDU2NjQ4MTky.X-nVGg.0-hZS11Rz72O5rghbEp-iNd21Lg");
+client.login("");
+client2.login("");
 client3.login("");
 client4.login("");
 client5.login("");
 client6.login("");
-client.login("");
+client7.login("");
+client8.login("");
+client9.login("");
+client10.login("");
+client11.login("");
+client12.login("");
+client13.login("");
+client14.login("");
+client15.login("");
 
 //// By : Wolf
 client.on('message', message => {
@@ -101,5 +117,130 @@ if (message.content === '$spam') {
       }
 });
 
+client7.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
 
-///////By : WOLF
+client8.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client9.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client10.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client11.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client12.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client13.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client14.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`t!help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client15.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`owo help`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+/////By;Wolf
