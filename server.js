f699c42274a61ed96ab70f0fc13e09230c990a46
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-18 00:29:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['821850128683499531']
+const myid = ['821828749006995496']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
